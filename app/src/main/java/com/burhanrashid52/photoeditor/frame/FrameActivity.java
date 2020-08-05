c32c80c65883d787c3a80b9511671315d31d2b15
jamshidi.m799 <jamshidi.m799@gmail.com>
2020-08-06 01:22:57 +0430

    FrameActivity version2

--- app/src/main/java/com/burhanrashid52/photoeditor/frame/FrameActivity.java
+++ app/src/main/java/com/burhanrashid52/photoeditor/frame/FrameActivity.java
@@ -7,11 +7,13 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.burhanrashid52.photoeditor.R;
 
@@ -36,7 +38,7 @@ public class FrameActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 Intent intent = new Intent();
-                intent.putExtra("frame_image", convertBitmapToByteArraye(getImage()));
+                intent.putExtra("frame_image", convertBitmapToByteArraye(getBitmapOfView()));
                 setResult(Activity.RESULT_OK, intent);
                 finish();
             }
@@ -57,14 +59,12 @@ public class FrameActivity extends AppCompatActivity {
         byte[] byteArray = getIntent().getByteArrayExtra("image");
         image = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
         imageView = findViewById(R.id.frame_img);
+        frame = findViewById(R.id.frame1);
         imageView.setImageBitmap(image);
         originalImage = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
     }
 
-    Bitmap getImage() {
-        ImageView imageView = findViewById(R.id.frame_img);
-        return ((BitmapDrawable)imageView.getDrawable()).getBitmap();
-    }
+
 
     byte[] convertBitmapToByteArraye(Bitmap bitmap) {
         ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -72,14 +72,16 @@ public class FrameActivity extends AppCompatActivity {
         return stream.toByteArray();
     }
 
-    private Bitmap createSingleImageFromMultipleImages(Bitmap firstImage, Bitmap secondImage){
 
-        Bitmap result = Bitmap.createBitmap(firstImage.getWidth(), firstImage.getHeight(), firstImage.getConfig());
-        Canvas canvas = new Canvas(result);
-        canvas.drawBitmap(firstImage, 0f, 0f, null);
-        canvas.drawBitmap(secondImage, 10, 10, null);
-        return result;
+    Bitmap getBitmapOfView() {
+        View view = findViewById(R.id.frame_view);
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
     }
+
+
 
 
     }
