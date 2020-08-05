d29859c96eb30c33db001f18a8224b1590a60b6f
roodabeh <roodabesafavi@yahoo.com>
2020-08-05 23:11:46 +0430

    add crop buttons

--- app/src/main/java/com/burhanrashid52/photoeditor/CropActivity.java
+++ app/src/main/java/com/burhanrashid52/photoeditor/CropActivity.java
@@ -7,6 +7,8 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -22,22 +24,22 @@ public class CropActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         initData();
 
-        Button done = findViewById(R.id.crop_done);
+        TextView done = findViewById(R.id.crop_done_txt);
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
-                intent.putExtra("cropped_image", convertBitmapToByteArraye(getCroppedImage()));
+                intent.putExtra("image", convertBitmapToByteArraye(getCroppedImage()));
                 setResult(Activity.RESULT_OK, intent);
                 finish();
             }
         });
 
-        Button cancel = findViewById(R.id.crop_cancel);
+        ImageView cancel = findViewById(R.id.crop_cancel);
         cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
